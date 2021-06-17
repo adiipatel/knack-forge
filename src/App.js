912d62376745e68e11dff95e0a2e1cf62edac7a9
Aditya Patel <adipatel173gmail.com>
2021-06-17 18:29:15 -0400

    Added REST API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import Banner from './Banner/Banner';
@@ -18,176 +18,190 @@ import { BsFolderCheck } from "react-icons/bs";
 import { RiTeamLine } from "react-icons/ri";
 
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <Banner />
+class App extends Component {
 
-      <Product
-        id="product" 
-        no="01" 
-        para1="Our success rests in the relationship between people"
-        head1="Products &amp; people"
-        img="product"
-        h1="Constant evolution, constant adaptation"
-        p1="We know what it takes to help your business prepare
-        for the next generation of digital transformations. At 
-        KnackForge, we provide a comprehensive suite of
-        solutions and services that are technologically 
-        superior and commercially viable."
-      />
-      
-      <Vision 
-        text={["Let's work ",<span className='bold'>together </span>,
-        "to turn your ", <span className='bold'>company's</span>," vision into ",
-        <span className='bold'>software</span>," solution with ",
-        <span className='bold'>staying</span>," power."]}
-      />
+  constructor(props){
+    super(props);
+    this.state = {
+      users: [],
+      isLoaded: false
+    }
+  }
 
-      <Platform />
-      <Product
-        id="solution"
-        no="02" 
-        para1="Choose from our service and find the tools you need"
-        head1="Solution &amp; technology"
-        img="technology"
-        h1="Your potential, our passion"
-        p1="We know what it takes to help your business prepare
-        for the next generation of digital transformations. At 
-        KnackForge, we provide a comprehensive suite of
-        solutions and services that are technologically 
-        superior and commercially viable."
-      />
+  componentDidMount() {
 
-      <div className="techs">
-          <Tech 
-            icon={[<BsCloudUpload size='50px'/>]}
-            head="Cloud Computing &amp; Infrastructure"
-            para="KnackForge has helped many 
-            clients reduce overhead and 
-            improve reliability with
-            Amazon Web Services." 
-          />
-          <Tech 
-            icon={[<FiGlobe size='50px'/>]}
-            head="Web Application Development"
-            para="KnackForge develops responsive
-            web applications that work. From
-            Drupal and Wordpress to custom 
-            web applications our team will
-            excel in your environment." 
-          />
-          <Tech 
-            icon={[<RiSettings3Line size='50px'/>]}
-            head="Cross Platform Mobile App Development"
-            para="Secure the KnackForge team
-            to perform mobile development
-            for Android and/or Apple iOS." 
-          />
-          <Tech 
-            icon={[<AiOutlineMessage size='50px'/>]}
-            head="Consulting Services"
-            para="Secure the KnackForge team
-            to perform mobile 
-            development for Android
-            and/or Apple iOS." 
-          />
-          <Tech 
-            icon={[<BsFolderCheck size='50px'/>]}
-            head="Enterprice Data Warehouse"
-            para="As companies like yours thrive
-            and grow in today's cometitive
-            business environment, they
-            need to access key metrics to 
-            drive good decisions." 
-          />
-          <Tech
-            icon={[<RiTeamLine size='50px'/>]} 
-            head="Staff Augmentation &amp; Dedicated Teams"
-            para="KnackForge serves the unique
-            requirements of your company
-            by either serving as your 
-            complete development solution
-            or to simply expand your staff." 
-          />
-      </div>
+    fetch('https://my-json-server.typicode.com/adiipatel/knack-forge-jsonserver/users')
+      .then(res => res.json())
+      .then(json => {
+          this.setState({
+              isLoaded: true,
+              users: json
+          })
+      });
 
-      <Vision 
-        text={["Need a ",<span className='bold'>dependable </span>,
-        "team? We're ", <span className='bold'>here for it.</span>," Bringing out ",
-        <span className='bold'>the best</span>," in each other! "]}
-      />
+  }
 
-      <Product 
-        id="team"
-        no="03" 
-        para1="Technology is great. The minds that created it are greater."
-        head1="Team &amp; Leadership"
-        img="team"
-        h1="Bringing out, the best!"
-        p1="We know what it takes to help your business prepare
-        for the next generation of digital transformations. At 
-        KnackForge, we provide a comprehensive suite of
-        solutions and services that are technologically 
-        superior and commercially viable."
-      />
-      
-      <div className="people">
-        <Team 
-          img="peop1"
-          name="Subhash Chandra"
-          position="President"
-        />
-        <Team 
-          img="peop2"
-          name="John Snow"
-          position="CEO"
-        />
-        <Team 
-          img="peop3"
-          name="Tony Stark"
-          position="Business Development"
-        />
-        <Team 
-          img="peop4"
-          name="Eddie House"
-          position="Associate Vice President"
-        />
-        <Team 
-          img="peop5"
-          name="Greygory Lawson"
-          position="Customer Experience"
-        />
-        <Team 
-          img="peop6"
-          name="Rose Morrow"
-          position="Human Resources"
-        />
-      </div>
+  render () {
 
-      <Vision 
-        text={["KnackForge is ",<span className='bold'>working </span>,
-        "on ", <span className='bold'>software</span>," that brings all the ",
-        <span className='bold'>pieces together</span>]}
-      />
+    var { isLoaded, users } = this.state;
 
-      <Product 
-        id="news"
-        no="04" 
-        para1="Click on to see the real-life impact of our software"
-        head1="News &amp; Blog"
-        img="news"
-        h1="Cross-platform serverless app using nativescripts and services"
-        p1="I wanted to demonstrate how easy to build a native
-        looking cross-platform app using AWS services without actually 
-        having your server."
-        />
+    if (!isLoaded) {
+      return <h1> Loading... </h1>
+    }
 
-      <Footer />
+    else { 
 
-    </>   
-  );
+      return (
+          <>
+            <Navbar />
+            <Banner />
+
+            <Product
+              id="product" 
+              no="01" 
+              para1="Our success rests in the relationship between people"
+              head1="Products &amp; people"
+              img="product"
+              h1="Constant evolution, constant adaptation"
+              p1="We know what it takes to help your business prepare
+              for the next generation of digital transformations. At 
+              KnackForge, we provide a comprehensive suite of
+              solutions and services that are technologically 
+              superior and commercially viable."
+            />
+            
+            <Vision 
+              text={["Let's work ",<span className='bold'>together </span>,
+              "to turn your ", <span className='bold'>company's</span>," vision into ",
+              <span className='bold'>software</span>," solution with ",
+              <span className='bold'>staying</span>," power."]}
+            />
+
+            <Platform />
+            <Product
+              id="solution"
+              no="02" 
+              para1="Choose from our service and find the tools you need"
+              head1="Solution &amp; technology"
+              img="technology"
+              h1="Your potential, our passion"
+              p1="We know what it takes to help your business prepare
+              for the next generation of digital transformations. At 
+              KnackForge, we provide a comprehensive suite of
+              solutions and services that are technologically 
+              superior and commercially viable."
+            />
+
+            <div className="techs">
+                <Tech 
+                  icon={[<BsCloudUpload size='50px'/>]}
+                  head="Cloud Computing &amp; Infrastructure"
+                  para="KnackForge has helped many 
+                  clients reduce overhead and 
+                  improve reliability with
+                  Amazon Web Services." 
+                />
+                <Tech 
+                  icon={[<FiGlobe size='50px'/>]}
+                  head="Web Application Development"
+                  para="KnackForge develops responsive
+                  web applications that work. From
+                  Drupal and Wordpress to custom 
+                  web applications our team will
+                  excel in your environment." 
+                />
+                <Tech 
+                  icon={[<RiSettings3Line size='50px'/>]}
+                  head="Cross Platform Mobile App Development"
+                  para="Secure the KnackForge team
+                  to perform mobile development
+                  for Android and/or Apple iOS." 
+                />
+                <Tech 
+                  icon={[<AiOutlineMessage size='50px'/>]}
+                  head="Consulting Services"
+                  para="Secure the KnackForge team
+                  to perform mobile 
+                  development for Android
+                  and/or Apple iOS." 
+                />
+                <Tech 
+                  icon={[<BsFolderCheck size='50px'/>]}
+                  head="Enterprice Data Warehouse"
+                  para="As companies like yours thrive
+                  and grow in today's cometitive
+                  business environment, they
+                  need to access key metrics to 
+                  drive good decisions." 
+                />
+                <Tech
+                  icon={[<RiTeamLine size='50px'/>]} 
+                  head="Staff Augmentation &amp; Dedicated Teams"
+                  para="KnackForge serves the unique
+                  requirements of your company
+                  by either serving as your 
+                  complete development solution
+                  or to simply expand your staff." 
+                />
+            </div>
+
+            <Vision 
+              text={["Need a ",<span className='bold'>dependable </span>,
+              "team? We're ", <span className='bold'>here for it.</span>," Bringing out ",
+              <span className='bold'>the best</span>," in each other! "]}
+            />
+
+            <Product 
+              id="team"
+              no="03" 
+              para1="Technology is great. The minds that created it are greater."
+              head1="Team &amp; Leadership"
+              img="team"
+              h1="Bringing out, the best!"
+              p1="We know what it takes to help your business prepare
+              for the next generation of digital transformations. At 
+              KnackForge, we provide a comprehensive suite of
+              solutions and services that are technologically 
+              superior and commercially viable."
+            />
+            
+            <div className="people">
+              {users.map(user => (
+                  <Team key={user.id}
+                    img={user.url}
+                    name={user.name}
+                    position={user.position}
+                  />
+              ))};
+                
+              
+            </div>
+
+            <Vision 
+              text={["KnackForge is ",<span className='bold'>working </span>,
+              "on ", <span className='bold'>software</span>," that brings all the ",
+              <span className='bold'>pieces together</span>]}
+            />
+
+            <Product 
+              id="news"
+              no="04" 
+              para1="Click on to see the real-life impact of our software"
+              head1="News &amp; Blog"
+              img="news"
+              h1="Cross-platform serverless app using nativescripts and services"
+              p1="I wanted to demonstrate how easy to build a native
+              looking cross-platform app using AWS services without actually 
+              having your server."
+              />
+
+            <Footer />
+
+          </>   
+      );
+
+    } 
+  }
 }
 
 export default App;
--- src/Team/Team.js
+++ src/Team/Team.js
@@ -5,7 +5,7 @@ const Team = props => {
     return (
         <div className="team">
             <div className="peop">
-              <img className="peop-img" src={`/images/${props.img}.jpg`} alt="Product" width="300px"></img>
+              <img className="peop-img" src={`${props.img}`} alt="Product" width="300px"></img>
             </div> 
             <h1>{props.name}</h1>
             <p className="grey">{props.position}</p>   
